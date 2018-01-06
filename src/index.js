1e62fcb0eb13a0bd7dce77c529c4fa09b816d864
= <=>
2018-01-06 17:48:48 -0500

    'ms'

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDom from "react-dom";
 import Main from "./components/Main";
 import "./components/styles/index.css";
 
+// https://stackoverflow.com/questions/31852933/why-es6-react-component-works-only-with-export-default/31853887#31853887
+
+
 const root = document.querySelector("#container");
  
-
 ReactDom.render(
     <Main/>,
     root
